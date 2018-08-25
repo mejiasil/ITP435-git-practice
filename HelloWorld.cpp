92a1ad77c30abf1fb6b0e5f6b526660b948c1a97
Juan Mejia <mejiasil@usc.edu>
2018-08-25 13:25:13 -0700

    initial commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+//
+//  HelloWorld.cpp
+//  
+//
+//  Created by Juan Mejia on 8/25/18.
+//
+
+#include "HelloWorld.hpp"
+
+# include <iostream>
+
+int main()
+{
+    std::cout << "HelloWorld!" << std::endl;
+}
