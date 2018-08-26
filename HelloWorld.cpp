639fbf329fbb26679db1ce8f1a6202bf2d67d3e2
Juan Mejia <mejiasil@usc.edu>
2018-08-25 20:23:48 -0700

    small change

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,11 +5,10 @@
 //  Created by Juan Mejia on 8/25/18.
 //
 
-#include "HelloWorld.hpp"
 
 # include <iostream>
 
 int main()
 {
-    std::cout << "FIGHT ON!" << std::endl;
+    std::cout << "FIGHT ON!!" << std::endl;
 }
